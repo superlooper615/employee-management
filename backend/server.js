5252d9d84b6d01bcadcebb4ae12b63dc933108af
Daryl <jdsantil@cougarnet.uh.edu>
2020-11-14 20:53:32 -0600

    restart

--- backend/server.js
+++ backend/server.js
@@ -26,13 +26,13 @@ app.set('view engine', '.hbs');
 app.set('views',path.join(__dirname,'views'));
 
 // Database connection imports
-const db = require('./config/db.config.js');
-// force: true will drop the table if it already exists
-db.sequelize.sync({force: true}).then(() => {
-  console.log('Databse successfuly initialized!');
-}).catch((err) => {
-  console.log(err, "Something went wrong with the Database Update!");
-});
+// const db = require('./config/db.config.js');
+// // force: true will drop the table if it already exists
+// db.sequelize.sync({force: true}).then(() => {
+//   console.log('Databse successfuly initialized!');
+// }).catch((err) => {
+//   console.log(err, "Something went wrong with the Database Update!");
+// });
 
 import passport from  'passport';
 //load passport strategies
